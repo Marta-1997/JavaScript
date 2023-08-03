3ab3b17f9c9b46a618887f4b5ffca3f43a35ecf9
Marta-1997 <131486711+Marta-1997@users.noreply.github.com>
2023-08-03 20:23:24 +0200

    Add files via upload

--- JavaScript/Uship/js/scriptCtto.js
+++ JavaScript/Uship/js/scriptCtto.js
@@ -1,12 +1,25 @@
-function validarForm(){
-    alert("Validando...");
+
+
+
+function valida_envia(){
+  
+  if (document.fvalida.nombre.value.length==0){
+       alert("Tiene que escribir sus datos")
+       document.fvalida.nombre.focus()
+       return 0;
   }
+
   
-  window.addEventListener("load",()=>{
-    let boton=document.querySelector("#boton");
-    boton.addEventListener("click", validarForm);
-    cambiarImg();
-  });
   
-  window.addEventListener("load", ()=>{
-})
+
+ 
+  if (document.fvalida.interes.selectedIndex==0){
+       alert("Debe seleccionar un motivo de su contacto.")
+       document.fvalida.interes.focus()
+       return 0;
+  }
+
+  
+  alert("Muchas gracias por enviar el formulario");
+  document.fvalida.submit();
+}
